a88b0515f6ca1b070c6d78c017d4cae653212717
Cathy Sheng <cathysheng06@berkeley.edu>
2026-04-28 11:04:49 -0700

    changed the router issue

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { ConnectButton } from "@/components/ConnectButton";
@@ -22,9 +22,11 @@ export default function RegisterPage() {
     blockAiTraining: false,
   });
 
-  if (isSuccess && photoData) {
-    router.push(`/photo/${photoData.imageHash}`);
-  }
+  useEffect(() => {
+    if (isSuccess && photoData) {
+      router.push(`/photo/${photoData.imageHash}`);
+    }
+  }, [isSuccess, photoData, router]);
 
   const canSubmit = isConnected && photoData && !isPending && !isConfirming;
 
